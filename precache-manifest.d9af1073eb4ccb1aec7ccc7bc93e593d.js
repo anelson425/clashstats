de311e7ecc02a9c2d32197533e658cfada35b494
Andy Nelson <anelson425@gmail.com>
2019-10-13 21:13:27 -0500

    Updates

---
+++ precache-manifest.d9af1073eb4ccb1aec7ccc7bc93e593d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756ca3b6cad2171b9f12535495d7bbf9",
+    "url": "/clashstats/index.html"
+  },
+  {
+    "revision": "9b91a6491a0a36bdace6",
+    "url": "/clashstats/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "26ef2599d5ac2d66cf68",
+    "url": "/clashstats/static/js/2.52c1be88.chunk.js"
+  },
+  {
+    "revision": "9b91a6491a0a36bdace6",
+    "url": "/clashstats/static/js/main.f3482bca.chunk.js"
+  },
+  {
+    "revision": "a894b4aa227fd4ea96b6",
+    "url": "/clashstats/static/js/runtime-main.729016d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clashstats/static/media/logo.25bf045c.svg"
+  }
+]);
